a0d154ed98e4153e604ac756f2c0e61fc3caf289
Frederik P <frepbh@gmail.com>
2022-05-02 20:51:30 +0200

    Seperated majority of code into its own library.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-mod ico_writer;
-mod png;
-
 use std::path::PathBuf;
+use std::{error::Error, result::Result};
 
-fn main() -> png::Result<()> {
+use ico_rs::png::write_ico;
+
+fn main() -> Result<(), Box<dyn Error>> {
 	let image = PathBuf::from(std::env::args().nth(1).ok_or("Image path was invalid.")?);
-	let image_str = image.to_str().unwrap();
-	let parser = png::PngParser::new();
-	let png = parser.parse_header(image_str)?;
-	ico_writer::write_ico(
-		format!("./{}", image.file_name().unwrap().to_str().unwrap()),
-		png,
+	let image_str = image.to_str().ok_or("Could not convert")?;
+	write_ico(
+		format!(
+			"./{}",
+			image
+				.file_name()
+				.ok_or("Could not get image file name.")?
+				.to_str()
+				.ok_or("Could not convert OsStr to str")?
+		),
 		image_str,
 	)?;
 	Ok(())
--- src/png/mod.rs
+++
@@ -1,8 +0,0 @@
-mod chunk;
-mod encoded_png;
-pub(crate) mod png_meta;
-mod png_parser;
-
-pub use png_parser::PngParser;
-
-pub type Result<T> = std::result::Result<T, &'static str>;
